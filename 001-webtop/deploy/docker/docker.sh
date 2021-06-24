6d7fd09d6d3a5c04965d7e207bdeec01af44247a
Onur GURSOY <onurgursoy@mail.com>
2021-06-24 23:13:38 +0300

    initial commit

---
+++ 001-webtop/deploy/docker/docker.sh
@@ -0,0 +1,10 @@
+docker run -d \
+  --name=webtop \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -p 3000:3000 \
+  -v /path/to/data:/config \
+  --shm-size="1gb" \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/webtop
